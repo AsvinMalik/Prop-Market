beb14b705c0137e57855199e0d9be385b8988838
Asvin Malik <asvin24102@iiitnr.edu.in>
2026-05-30 18:46:22 +0530

    Use root base path for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/Prop-Market/',
+  base: command === 'build' ? '/Prop-Market/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
